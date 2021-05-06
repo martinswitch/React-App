2201210c63380d5f427ace4457e2fd842b88de15
Executive Producer <martin.d.kenny@gmail.com>
2021-05-06 14:58:57 -0400

    Welcome,About, Contact Pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { createContext } from 'react';
+import Home from "./Component/Home"
+import About from "./Component/About"
+import Contact from "./Component/Contact"
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+// we have two types of components
+// 1 sfc=> statel ess funcional component
+// 2 class based component
+
+class App extends Component {
+  state = { 
+    firstName:'Martin',
+    phone:'22323'
+   }
+  render() { 
+    return (  <div>
+      {/* <Home /> */}
+
+      <About />
+
+      <Contact />
+
+
+
+
+    </div>);
+  }
 }
-
-export default App;
+ 
+export default App; 
